aaccb8aa4c1b4019a2f8ceb31e6d8650d1b94e28
Sigri44 <sigri44@hotmail.fr>
2017-03-26 14:27:52 +0200

    Correction synthaxe PHP et indentation

--- admin/potins.php
+++ admin/potins.php
@@ -9,55 +9,57 @@
     
     <table align="center" border="0">
 			<tr>
+                <td align=center bgcolor="#FEDB71" width="10%"><font face="Trebuchet MS, Comic Sans MS" size=2>
+                    <b>Date</b>
+                    </font>
+                </td>
 				<td align=center bgcolor="#FEDB71" width="10%"><font face="Trebuchet MS, Comic Sans MS" size=2>
-                <b>Date</b>
-				</font></td>
-				<td align=center bgcolor="#FEDB71" width="10%"><font face="Trebuchet MS, Comic Sans MS" size=2>
-	            <b>Auteur</b>
-				</font></td>
+                    <b>Auteur</b>
+                    </font>
+                </td>
 				<td align=center bgcolor="#FEDB71" width="70%"><font face="Trebuchet MS, Comic Sans MS" size=2>
-	            <b>Potin</b>
-				</font></td>
+                    <b>Potin</b>
+                    </font>
+                </td>
 				<td align=center bgcolor="#FEDB71" width="70%"><font face="Trebuchet MS, Comic Sans MS" size=2>
-	            <b>Supprimer</b>
-				</font></td>
-				</tr>
-<?php
+                    <b>Supprimer</b>
+                    </font>
+                </td>
+            </tr>
+        <?php
+            $ligne=1;
+            if ($count=="") {
+                $count=0;
+            }
 
-$ligne=1;
-if ($count=="") {
-	$count=0;
-}
-
-$query = "SELECT * FROM potins GROUP BY Id desc LIMIT $count, 10"; 
+            $query = "SELECT * FROM potins GROUP BY Id desc LIMIT $count, 10"; 
 
 
-$result = mysql_query($query,$conexion);
-// tant qu'il y a des fiches
-while ($val = mysql_fetch_array($result)) {
-	?>
+            $result = mysql_query($query,$conexion);
+            // tant qu'il y a des fiches
+            while ($val = mysql_fetch_array($result)) {
+        ?>
 		<tr>
 			<td align=center bgcolor="<?php	if ($ligne==1) { echo "F2F2F2"; } else {  echo "D6D6D6";  } ?>" width="10%"><font face="Trebuchet MS, Comic Sans MS" size=2><a><?php echo $val["Date"]; ?></font></td>
 			<td align=center bgcolor="<?php	if ($ligne==1) { echo "F2F2F2"; } else {  echo "D6D6D6";  } ?>" width="10%"><font face="Trebuchet MS, Comic Sans MS" size=2><?php echo $val["nom"]; ?></font></td>
 			<td align=center bgcolor="<?php	if ($ligne==1) { echo "F2F2F2"; } else {  echo "D6D6D6";  } ?>" width="70%"><font face="Trebuchet MS, Comic Sans MS" size=2><?php echo $val["Potins"]; ?></font></td>
 			<td align=center bgcolor="<?php	if ($ligne==1) { echo "F2F2F2"; } else {  echo "D6D6D6";  } ?>" width="70%"><input type="checkbox" name="<?php echo $val["Id"]; ?>" value="ON"></td>
 		</tr>
-<?php
-
-	if ($ligne==1) {
-		$ligne=0;
-	} else {
-		$ligne=1;
-	}
-}
-?>
-</table><div align="right"><input type="submit" value="Supprimer" name="B1"></form>
-<a href="potins.php?potin=ok&count=<?php echo $count+10;?>" border=0>Précédents </a> 
-<?php
-    if ($count<>0) { 
-        echo " --- <a href=potins.php?potin=ok&count=".($count-10)." border=0> Suivants</a>";
-    }
-?>
+        <?php
+            if ($ligne==1) {
+                $ligne=0;
+            } else {
+                $ligne=1;
+            }
+        }
+        ?>
+        </table><div align="right"><input type="submit" value="Supprimer" name="B1"></form>
+        <a href="potins.php?potin=ok&count=<?php echo $count+10;?>" border=0>Précédents </a> 
+        <?php
+            if ($count<>0) { 
+                echo " --- <a href=potins.php?potin=ok&count=".($count-10)." border=0> Suivants</a>";
+            }
+        ?>
 <?php
          include("../aqua_bas.htm");
 ?>
